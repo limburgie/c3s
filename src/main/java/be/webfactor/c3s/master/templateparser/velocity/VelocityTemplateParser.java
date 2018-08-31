d2b1e69948b6e30b5dd93fd434b2834637451554
Peter Mesotten <peter.mesotten@aca-it.be>
2018-08-31 14:33:11 +0200

    #2: Implemented resource loading for Velocity templates

--- src/main/java/be/webfactor/c3s/master/templateparser/velocity/VelocityTemplateParser.java
+++ src/main/java/be/webfactor/c3s/master/templateparser/velocity/VelocityTemplateParser.java
@@ -1,10 +1,18 @@
 package be.webfactor.c3s.master.templateparser.velocity;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.resource.loader.URLResourceLoader;
 import org.apache.velocity.tools.ToolManager;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +23,24 @@ import be.webfactor.c3s.master.templateparser.TemplateParserException;
 @Service
 public class VelocityTemplateParser implements TemplateParser {
 
-	private VelocityEngine velocityEngine;
-
-	public VelocityTemplateParser() {
-		velocityEngine = new VelocityEngine();
-		velocityEngine.init();
-	}
-
 	public String parse(String templateName, String templateContents, Map<String, Object> context, String baseUrl) throws TemplateParserException {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.setProperty("resource.loader", "mine");
+		velocityEngine.setProperty("mine.resource.loader.instance", new URLResourceLoader() {
+			public synchronized InputStream getResourceStream(String name) throws ResourceNotFoundException {
+				if ("VM_global_library.vm".equals(name)) {
+					return super.getResourceStream(name);
+				}
+
+				try {
+					return new URI(baseUrl + "/" + name).toURL().openStream();
+				} catch (URISyntaxException | IOException e) {
+					return null;
+				}
+			}
+		});
+		velocityEngine.init();
+
 		VelocityContext velocityContext = new VelocityContext(context, new ToolManager().createContext());
 
 		StringWriter stringWriter = new StringWriter();
