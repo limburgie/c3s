41fcc9eaa2514b0fbdf3e00e0c00d4ea7cdcaa97
Peter Mesotten <peter.mesotten@aca-it.be>
2019-12-11 11:21:09 +0100

    #25: Setup basic SASS compilation settings

---
+++ src/main/java/be/webfactor/c3s/controller/sass/SassCompilationException.java
@@ -0,0 +1,8 @@
+package be.webfactor.c3s.controller.sass;
+
+public class SassCompilationException extends RuntimeException {
+
+	public SassCompilationException(Throwable cause) {
+		super(cause);
+	}
+}
