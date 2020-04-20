d8c7a36dd0067a1e92824b78b34e20620d5959b7
Peter Mesotten <peter.mesotten@aca-it.be>
2020-04-20 19:55:57 +0200

    #31: Add fallback for unicode content in i18n.properties

---
+++ src/main/java/be/webfactor/c3s/master/service/webserver/i18n/UTF8Control.java
@@ -0,0 +1,44 @@
+package be.webfactor.c3s.master.service.webserver.i18n;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class UTF8Control extends ResourceBundle.Control {
+
+	public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload) throws IOException {
+		String bundleName = toBundleName(baseName, locale);
+		String resourceName = toResourceName(bundleName, "properties");
+		ResourceBundle bundle = null;
+		InputStream stream = null;
+		if (reload) {
+			URL url = loader.getResource(resourceName);
+			if (url != null) {
+				URLConnection connection = url.openConnection();
+				if (connection != null) {
+					connection.setUseCaches(false);
+					stream = connection.getInputStream();
+				}
+			}
+		} else {
+			stream = loader.getResourceAsStream(resourceName);
+		}
+		if (stream != null) {
+			try {
+				bundle = new PropertyResourceBundle(new InputStreamReader(stream, StandardCharsets.UTF_8));
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			} finally {
+				stream.close();
+			}
+		}
+		return bundle;
+	}
+}
