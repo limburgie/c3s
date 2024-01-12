740d07d0bfcd0ba6e4d70955ee4ee7f68f984509
Peter Mesotten <peter@web-factor.be>
2024-01-12 21:20:31 +0100

    #41: Fixed problem with index page and extended uri helper to add language

--- src/main/java/be/webfactor/c3s/renderer/UriHelper.java
+++ src/main/java/be/webfactor/c3s/renderer/UriHelper.java
@@ -1,30 +1,33 @@
 package be.webfactor.c3s.renderer;
 
-import be.webfactor.c3s.master.domain.LocaleContext;
 import be.webfactor.c3s.master.domain.LocationThreadLocal;
 import be.webfactor.c3s.master.domain.Page;
 
 public class UriHelper {
 
     public String ofIndex() {
-        LocaleContext localeContext = LocationThreadLocal.getLocaleContext();
+        return ofIndex(LocationThreadLocal.getLocaleContext().getLocale().getLanguage());
+    }
 
+    public String ofIndex(String language) {
         StringBuilder sb = new StringBuilder("/");
 
-        if (localeContext.isUriLocalePrefixed()) {
-            sb.append(localeContext.getLocale().getLanguage());
+        if (be.webfactor.c3s.master.domain.LocationThreadLocal.getLocaleContext().isUriLocalePrefixed()) {
+            sb.append(language);
         }
 
         return sb.toString();
     }
 
     public String of(Page page) {
-        LocaleContext localeContext = LocationThreadLocal.getLocaleContext();
+        return of(page, LocationThreadLocal.getLocaleContext().getLocale().getLanguage());
+    }
 
+    public String of(Page page, String language) {
         StringBuilder sb = new StringBuilder("/");
 
-        if (localeContext.isUriLocalePrefixed()) {
-            sb.append(localeContext.getLocale().getLanguage()).append("/");
+        if (LocationThreadLocal.getLocaleContext().isUriLocalePrefixed()) {
+            sb.append(language).append("/");
         }
 
         sb.append(page.getFriendlyUrl());
